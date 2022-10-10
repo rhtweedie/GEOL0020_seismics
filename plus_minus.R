42f386a4b6e301c8d0e41c24f655cdf0cbbd50e7
Heather Tweedie <rhtweedie12@gmail.com>
2022-10-10 17:07:09 +0100

    Rename data files, add plots

--- plus_minus.R
+++ plus_minus.R
@@ -1,1 +1,21 @@
-read.csv("test.csv")
+library(ggplot2)
+library(tibble)
+library(dplyr)
+
+# load data
+ns_data = read.csv("data/ns_data.csv")
+ns_data = tibble(ns_data)
+ew_data = read.csv("data/ew_data.csv")
+ew_data = tibble(ew_data)
+
+ggplot(ns_data) + 
+  geom_point(aes(x = Offset, y = TA), size = 2, color = 'red') +
+  geom_point(aes(x = Offset, y = TB), size = 2, color = 'blue')
+
+ns_data %>% mutate(
+  ta_minus_tb = TA - TB
+)
+
+ggplot(ns_data) +
+  geom_point(aes(x = Offset, y = ta_minus_tb))
+
