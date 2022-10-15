d21954802a33cb97a9a57260e4879abfd152400e
Heather Tweedie <rhtweedie12@gmail.com>
2022-10-15 12:19:50 +0100

    Plot offset vs tA-tB and find gradient

--- plus_minus.R
+++ plus_minus.R
@@ -2,10 +2,12 @@ library(ggplot2)
 library(tibble)
 library(dplyr)
 
+
 # define variables
 data_to_load = "data/ns_data.csv"
 tAB = 0.06087
 tBA = 0.07547
+V1 = 1000 #calculated by hand from direct wave data
 
 # load data
 data = read.csv(data_to_load)
@@ -14,14 +16,27 @@ data = tibble(data)
 # find mean travel time between points A and B
 mean_tAB = mean(c(tAB, tBA))
 
+# plot offset vs travel times
 ggplot(data) + 
   geom_point(aes(x = offset, y = tA), size = 2, color = 'red') +
   geom_point(aes(x = offset, y = tB), size = 2, color = 'blue')
 
-# find tA - tB
-data = data %>% mutate(
+# find tA-tB
+minus_data = data %>% mutate(
   tA_minus_tB = tA - tB
 )
 
-ggplot(data) +
-  geom_point(aes(x = offset, y = ))
+# plot offset vs tA-tB (minus)
+ggplot(minus_data, aes(x = offset, y = tA_minus_tB)) +
+  geom_point() +
+  geom_smooth(aes(x = offset, y = tA_minus_tB), method = lm, se = FALSE) +
+  xlab("Offset (m)") +
+  ylab("tA - tB") +
+  ggtitle("tA - tB against Offset")
+
+# find and extract gradient of line of best fit
+model = lm(minus_data$tA_minus_tB ~ minus_data$offset)
+gradient = model$coefficients[2]
+
+# calculate V2
+V2 = 2/gradient
