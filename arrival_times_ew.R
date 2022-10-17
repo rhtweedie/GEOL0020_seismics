532bc2fb4ae5ea5c95874978aaddf3aa31b2c7df
Heather Tweedie <rhtweedie12@gmail.com>
2022-10-17 16:47:46 +0100

    Add lines of best fit

--- arrival_times_ew.R
+++ arrival_times_ew.R
@@ -6,32 +6,32 @@ data_to_load <- "data/greenwich_seismics_ew.csv"
 data <- read.csv(data_to_load)
 data <- tibble(data)
 
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
 ggplot(data) +
-  geom_point(aes(x = offset, y = shot_0), color = 'red') +
-    #geom_smooth(data = data[1:60,], aes(x = offset, y = shot_0), color = 'red', method = lm, se = FALSE) +
-  geom_point(aes(x = offset, y = shot_1), color = 'blue') +
-    #geom_smooth(data = data[1:5,], aes(x = offset, y = shot_1), color = 'blue', method = lm, se = FALSE) +
-    #geom_smooth(data = data[5:60,], aes(x = offset, y = shot_1), color = 'blue', method = lm, se = FALSE) +
-  geom_point(aes(x = offset, y = shot_2), color = 'green') +
-    #geom_smooth(data = data[1:5,], aes(x = offset, y = shot_2), color = 'green', method = lm, se = FALSE) +
-    #geom_smooth(data = data[5:13,], aes(x = offset, y = shot_2), color = 'green', method = lm, se = FALSE) +
-    #geom_smooth(data = data[13:16,], aes(x = offset, y = shot_2), color = 'green', method = lm, se = FALSE) +
-    #geom_smooth(data = data[16:60,], aes(x = offset, y = shot_2), color = 'green', method = lm, se = FALSE) +
-  geom_point(aes(x = offset, y = shot_3), color = 'yellow') +
-    #geom_smooth(data = data[1:22,], aes(x = offset, y = shot_3), color = 'yellow', method = lm, se = FALSE) +
-    #geom_smooth(data = data[22:25,], aes(x = offset, y = shot_3), color = 'yellow', method = lm, se = FALSE) +
-    #geom_smooth(data = data[25:28,], aes(x = offset, y = shot_3), color = 'yellow', method = lm, se = FALSE) +
-    #geom_smooth(data = data[28:60,], aes(x = offset, y = shot_3), color = 'yellow', method = lm, se = FALSE) +
-  geom_point(aes(x = offset, y = shot_4), color = 'orange') +
-    #geom_smooth(data = data[1:34,], aes(x = offset, y = shot_4), color = 'orange', method = lm, se = FALSE) +
-    #geom_smooth(data = data[34:37,], aes(x = offset, y = shot_4), color = 'orange', method = lm, se = FALSE) +
-    #geom_smooth(data = data[37:40,], aes(x = offset, y = shot_4), color = 'orange', method = lm, se = FALSE) +
-    #geom_smooth(data = data[40:60,], aes(x = offset, y = shot_4), color = 'orange', method = lm, se = FALSE) +
-  geom_point(aes(x = offset, y = shot_5), color = 'purple') +
-    #geom_smooth(data = data[1:46,], aes(x = offset, y = shot_5), color = 'purple', method = lm, se = FALSE) +
-    #geom_smooth(data = data[46:49,], aes(x = offset, y = shot_5), color = 'purple', method = lm, se = FALSE) +
-    #geom_smooth(data = data[49:53,], aes(x = offset, y = shot_5), color = 'purple', method = lm, se = FALSE) +
-    #geom_smooth(data = data[53:60,], aes(x = offset, y = shot_5), color = 'purple', method = lm, se = FALSE) +
+  geom_point(aes(x = offset, y = shot_0), color = cbPalette[1]) +
+    geom_smooth(data = data[1:48,], aes(x = offset, y = shot_0), color = cbPalette[1], method = lm, se = FALSE) +
+  geom_point(aes(x = offset, y = shot_1), color = cbPalette[2]) +
+    geom_smooth(data = data[1:13,], aes(x = offset, y = shot_1), color = cbPalette[2], method = lm, se = FALSE) +
+    geom_smooth(data = data[13:48,], aes(x = offset, y = shot_1), color = cbPalette[2], method = lm, se = FALSE) +
+  geom_point(aes(x = offset, y = shot_2), color = cbPalette[3]) +
+    geom_smooth(data = data[1:12,], aes(x = offset, y = shot_2), color = cbPalette[3], method = lm, se = FALSE) +
+    geom_smooth(data = data[12:26,], aes(x = offset, y = shot_2), color = cbPalette[3], method = lm, se = FALSE) +
+    geom_smooth(data = data[26:48,], aes(x = offset, y = shot_2), color = cbPalette[3], method = lm, se = FALSE) +
+  geom_point(aes(x = offset, y = shot_3), color = cbPalette[4]) +
+    geom_smooth(data = data[1:20,], aes(x = offset, y = shot_3), color = cbPalette[4], method = lm, se = FALSE) +
+    geom_smooth(data = data[20:24,], aes(x = offset, y = shot_3), color = cbPalette[4], method = lm, se = FALSE) +
+    geom_smooth(data = data[24:28,], aes(x = offset, y = shot_3), color = cbPalette[4], method = lm, se = FALSE) +
+    geom_smooth(data = data[28:48,], aes(x = offset, y = shot_3), color = cbPalette[4], method = lm, se = FALSE) +
+  geom_point(aes(x = offset, y = shot_4), color = cbPalette[5]) +
+    geom_smooth(data = data[1:28,], aes(x = offset, y = shot_4), color = cbPalette[5], method = lm, se = FALSE) +
+    geom_smooth(data = data[28:36,], aes(x = offset, y = shot_4), color = cbPalette[5], method = lm, se = FALSE) +
+    geom_smooth(data = data[36:39,], aes(x = offset, y = shot_4), color = cbPalette[5], method = lm, se = FALSE) +
+    geom_smooth(data = data[39:48,], aes(x = offset, y = shot_4), color = cbPalette[5], method = lm, se = FALSE) +
+  geom_point(aes(x = offset, y = shot_5), color = cbPalette[6]) +
+    geom_smooth(data = data[1:46,], aes(x = offset, y = shot_5), color = cbPalette[6], method = lm, se = FALSE) +
+    geom_smooth(data = data[46:48,], aes(x = offset, y = shot_5), color = cbPalette[6], method = lm, se = FALSE) +
   xlab('Offset, m') +
   ylab('Wave arrival time, s') +
   ggtitle('Arrival times for each shot and geophone location')
+
