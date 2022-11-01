1b471661d8d73f677e9020fa446f859889163160
Heather Tweedie <rhtweedie12@gmail.com>
2022-11-01 15:32:29 +0000

    Calculate and plot depth

--- plus_minus.R
+++ plus_minus.R
@@ -6,7 +6,7 @@ library(dplyr)
 # define variables
 tAB <-0.06087
 tBA <- 0.07547
-V1 <- 1000 #calculated from direct wave
+V1 <- 310 #calculated from direct wave
 
 # read data
 data_to_load <- "data/ns_data.csv"
@@ -21,9 +21,14 @@ ggplot(data) +
   geom_point(aes(x = offset, y = tA), size = 2, color = 'red') +
   geom_point(aes(x = offset, y = tB), size = 2, color = 'blue')
 
-# find tA-tB
+# find minus time (tA-tB)
 minus_data <- data %>% mutate(
   tA_minus_tB = tA - tB
+)
+
+# find plus time (tA + tB - tAB)
+plus_minus_data <- minus_data %>% mutate(
+  tA_plus_tB_minus_tAB = tA + tB  - mean_tAB
 )
 
 # plot offset vs tA-tB (minus)
@@ -42,10 +47,27 @@ model_2 <- lm(minus_data[9:38,]$tA_minus_tB ~ minus_data[9:38,]$offset)
 gradient_2 <- model_2$coefficients[2]
 
 # calculate V2
-V2 <- 2/gradient_1
+V2_1 <- 2/gradient_1
+V2_2 <- 2/gradient_2
 
 # calculate depths at each geophone
-plus_minus_data <- minus_data %>% mutate(
-  depth = (V1*V2) / (2 * (V2^2 - V1^2)^(1/2)) * mean_tAB
-)
+plus_minus_data['depth'] <- NA
+for (i in 1:9) {
+  plus_minus_data$depth[i] <- (V1*V2_1) / (2 * (V2_1^2 - V1^2)^(1/2)) * plus_minus_data$tA_plus_tB_minus_tAB[i]
+}
+for (i in 10:38) {
+  plus_minus_data$depth[i] <- (V1*V2_2) / (2 * (V2_2^2 - V1^2)^(1/2)) * plus_minus_data$tA_plus_tB_minus_tAB[i]
+}
 
+ggplot(plus_minus_data, aes(x = offset, y = depth)) +
+  geom_line() +
+  ylim(3, 0) +
+  geom_hline(yintercept = 0) +
+  geom_label(label = 'SURFACE', x = 49, y = -0.1) +
+  ggtitle('Cross-Section of Depth of First Layer Boundary across Site of Suspected Barrow') +
+  xlab('Offset (m)') +
+  ylab('Depth (m)') +
+  annotate("rect", xmin = 17, xmax = 27, ymin = 0, ymax = 3, alpha = 0.2) +
+  annotate("text", label = 'Surface width of barrow', x = 38, y = 2.3, size = 5) +
+  annotate("segment", x = 22, xend = 30, y = 2, yend = 2.3)
+  
